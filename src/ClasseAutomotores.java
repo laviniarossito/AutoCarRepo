efaca934a961f3287a9b7fa8ac6bfed1e89e51fd
laviniarossito <laviniarossito.etec@gmail.com>
2025-08-29 09:32:54 -0300

    CRIADO: Atributos na classe pai.

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,21 @@
 
 public class ClasseAutomotores {
 
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+	public ClasseAutomotores(){
+		
+	}
+	
+	public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar){
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
+
 }
